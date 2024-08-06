7b1a14c537f7f4c0b947e20bd04bbcfe4583f105
believe <believeosawaru2@gmail.com>
2024-08-06 20:44:10 +0100

    vytcujg.

--- js/home.js
+++ js/home.js
@@ -35,7 +35,7 @@ async function fetchMessage() {
 
         document.getElementById("welcome-msg").textContent = message;
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
     }
 }
 
