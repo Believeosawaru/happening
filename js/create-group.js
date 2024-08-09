b89279273702312bb749ddc00408ff47ef7f9f32
believe <believeosawaru2@gmail.com>
2024-08-09 13:02:01 +0100

    grsfvlbf fz\b

---
+++ js/create-group.js
@@ -0,0 +1,69 @@
+const signUpUrl = "https://happening-api.onrender.com/api/v1/user/create-group";
+
+const button = document.getElementById("sign-up-btn");
+
+function disableBtn() {
+    button.disabled = true;
+    button.style.backgroundColor = "#FFECB3";
+    button.style.color = "black"
+}
+ 
+
+document.getElementById("sign-up").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const firstName = document.getElementById("first-name").value;
+    const lastName = document.getElementById("last-name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    disableBtn();
+
+    try {
+        const response = await fetch(signUpUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({firstName, lastName, email, password})
+        });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        document.getElementById("success-sign-up").innerHTML = "Sign Up Successful";
+        document.getElementById("success-sign-up").classList.add("success-sign-up");
+
+        button.style.cursor = "wait";
+
+        setTimeout(() => {
+            window.location.href = "log-in.html"
+        }, 2000)
+    } else {
+        const keys = Object.keys(data);
+
+        keys.forEach(key => {
+            const value = data[key]; 
+            
+            document.getElementById("failed").style.display = "block"
+            document.getElementById("failed").innerHTML = value;
+            document.getElementById("failed").classList.add("failed");
+
+            setTimeout(() => {
+                document.getElementById("failed").style.display = "none"
+            }, 3500)
+
+            button.disabled = false;
+          });
+
+        button.style.backgroundColor = "#FF4500";
+        button.style.color = "white"
+    }
+
+    } catch (error) {
+        document.getElementById("failed").style.display = "block"
+        document.getElementById("failed").innerHTML = "There Was An Error, Please Reload The Page";
+        document.getElementById("failed").classList.add("failed");
+        console.log(error);
+    }
+})
--- js/home.js
+++ js/home.js
@@ -23,7 +23,7 @@ async function fetchMessage() {
     }
 
     try {
-        const response = await fetch("https://happening-api.onrender.com/user/v1/home", {
+        const response = await fetch("https://happening-api.onrender.com/api/v1/user/home", {
             method: "GET",
             headers: {
                 "Authorization": `Bearer ${token}`
