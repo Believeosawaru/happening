bbc73b07faf0b6ad64e0ceac8d08d450908c23c5
believe <believeosawaru2@gmail.com>
2024-08-13 14:14:01 +0100

    iuytrfde6crwenwev

--- js/group-details.js
+++ js/group-details.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
             const generateInviteLink = async () => {
                 try {
-                    const response = await fetch(`https://happening-api.onrender.com/api/v1/user/group/${groupId}/generate-link`, {
+                    const response = await fetch(`https://happening-api.onrender.com/api/v1/user/group/${groupId}`, {
                         method: "POST",
                         headers: {
                             "Authorization": `Bearer ${token}`
