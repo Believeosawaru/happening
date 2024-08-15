7cad505e2c4efc0d28a1908438120846a032b374
believe <believeosawaru2@gmail.com>
2024-08-15 12:23:51 +0100

    jbvy hjfrgbuj fi4ogf

---
+++ js/join-group.js
@@ -0,0 +1,60 @@
+const token = localStorage.getItem("authToken");
+const urlParams = new URLSearchParams(window.location.search);
+const groupId = urlParams.get("groupId");
+
+const button = document.getElementById("sign-in-btn");
+
+function disableBtn() {
+    button.disabled = true;
+    button.style.backgroundColor = "#FFECB3";
+    button.style.color = "black";
+}
+
+document.getElementById("join-group").addEventListener("click", async () => {
+    disableBtn();
+
+    try {
+        const response = await fetch(`https://happening-api.onrender.com/api/v1/user/join-group/${groupId}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        document.getElementById("success-sign-up").innerHTML = "Request Successful";
+        document.getElementById("success-sign-up").classList.add("success-sign-up");
+
+        button.style.cursor = "wait";
+
+        setTimeout(() => {
+            window.location.href = "groups.html"
+        }, 2000)
+    } else {
+        const keys = Object.keys(data);
+
+        keys.forEach(key => {
+            const value = data[key]; 
+            
+            document.getElementById("failed").style.display = "block"
+            document.getElementById("failed").innerHTML = value;
+            document.getElementById("failed").classList.add("failed");
+            console.log(data)
+            setTimeout(() => {
+                document.getElementById("failed").style.display = "none"
+            }, 3500)
+
+            button.disabled = false;
+          });
+
+        button.style.backgroundColor = "#FF4500";
+        button.style.color = "white";
+    }
+
+    } catch (error) {
+        console.log(error);
+    }
+});
