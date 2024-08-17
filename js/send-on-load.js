6310e61695fbc963860a75b5c326a60ead92ff2c
believe <believeosawaru2@gmail.com>
2024-08-18 00:38:33 +0100

    LOIJ0GFVCY H

--- js/send-on-load.js
+++ js/send-on-load.js
@@ -1,4 +1,6 @@
 async function getEmail() {
+    const token = localStorage.getItem("authToken");
+
     try {
         const response = await fetch("https://happening-api.onrender.com/api/v1/auth/send-on-load", {
             method: "GET",
