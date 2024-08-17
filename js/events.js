cbea6c319a666864bf843bc0a81c265201e07c2e
believe <believeosawaru2@gmail.com>
2024-08-17 03:06:56 +0100

    yhgbv tyghyhuihjk

---
+++ js/events.js
@@ -0,0 +1,64 @@
+async function displayEvents() {
+    const token = localStorage.getItem("authToken");
+
+    if (!token) {
+        window.location.href = "log-in.html"
+    }
+
+    try {
+        const response = await fetch("https://happening-api.onrender.com/api/v1/user/events", {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        });
+
+        if (response.status == 401) {
+            window.location.href = "log-in.html"
+        }
+
+        const data = await response.json();
+
+        if (response.ok) {
+            const eventContainer = document.getElementById("hero");
+
+            texts = "";
+
+            data.message.forEach((event) => {
+                texts += `
+                <a href="event-details.html?groupId=${event._id}">
+                <div id="group-desc-div">
+                    <h3>${event.name}</h3>
+                    <p><i class="fa fa-map loc-i"></i> ${event.location}</p>
+                    <p>${event.time}</p>
+                    <p class="group-type">${event.type}</p>
+                </div>
+                </a>
+                `
+            });
+
+            eventContainer.innerHTML = texts;
+
+            
+
+        } else {
+            const keys = Object.keys(data);
+    
+            keys.forEach(key => {
+                const value = data[key]; 
+                
+                document.getElementById("failed").style.display = "block"
+                document.getElementById("failed").innerHTML = value;
+                document.getElementById("failed").classList.add("failed");
+    
+                setTimeout(() => {
+                    document.getElementById("failed").style.display = "none"
+                }, 3500)
+              });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+window.onload = displayEvents;
