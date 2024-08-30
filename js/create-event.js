dcf0a1d65fbe83bb461a98b8eb6aa39ce1ec5aed
believe <believeosawaru2@gmail.com>
2024-08-30 17:44:03 +0100

    nm wef nwer

--- js/create-event.js
+++ js/create-event.js
@@ -83,32 +83,6 @@ document.getElementById("create-event").addEventListener("submit", async (e) => 
     const location = document.getElementById("event-location").value;
     const type = document.getElementById("event-type").value;
 
-    console.log(time, timezone)
-
-    // const eventTime = new Date(time);
-
-    // const timeFormatter = new Intl.DateTimeFormat("en-US", {
-    //     timeZone: timezone,
-    //     year: "numeric",
-    //     month: "2-digit",
-    //     day: "2-digit",
-    //     hour: "2-digit",
-    //     minute: "2-digit",
-    //     second: "2-digit"
-    // });
-
-    // const parts = timeFormatter.formatToParts(eventTime);
-
-    // const formattedDate = parts.reduce((acc, part) => {
-    //     if (part.type !== "literal") {
-    //         acc[part.type] = part.value;
-    //     }
-
-    //     return acc;
-    // }, {});
-
-    // const dateInUTC = new Date(`${formattedDate.year}-${formattedDate.month}-${formattedDate.day}T${formattedDate.hour}:${formattedDate.minute}:${formattedDate.second}Z`)
-
     disableBtn();
 
     const loc = document.getElementById("event-location");
@@ -117,63 +91,63 @@ document.getElementById("create-event").addEventListener("submit", async (e) => 
         return console.log("Pick A Location");
     }
 
-    // try {
-    //     const response = await fetch(signUpUrl, {
-    //         method: "POST",
-    //         headers: {
-    //             "Authorization": `Bearer ${token}`,
-    //             "Content-Type": "application/json"
-    //         },
-    //         body: JSON.stringify({
-    //             name,
-    //             description,
-    //             date,
-    //             time: dateInUTC.toISOString(),
-    //             timezone,
-    //             location,
-    //             type
-    //         })
-    //     });
+    try {
+        const response = await fetch(signUpUrl, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name,
+                description,
+                date,
+                time,
+                timezone,
+                location,
+                type
+            })
+        });
 
-    // const data = await response.json();
+    const data = await response.json();
 
-    // if (response.ok) {
-    //     document.getElementById("success-sign-up").innerHTML = "Event Created Successfully";
-    //     document.getElementById("success-sign-up").classList.add("success-sign-up");
+    if (response.ok) {
+        document.getElementById("success-sign-up").innerHTML = "Event Created Successfully";
+        document.getElementById("success-sign-up").classList.add("success-sign-up");
 
-    //     button.style.cursor = "wait";
+        button.style.cursor = "wait";
 
-    //     setTimeout(() => {
-    //         window.location.href = "events.html"
-    //     }, 2000)
-    // } else {
-    //     const keys = Object.keys(data);
+        setTimeout(() => {
+            window.location.href = "events.html"
+        }, 2000)
+    } else {
+        const keys = Object.keys(data);
 
-    //     keys.forEach(key => {
-    //         const value = data[key]; 
+        keys.forEach(key => {
+            const value = data[key]; 
             
-    //         document.getElementById("failed").style.display = "block"
-    //         document.getElementById("failed").innerHTML = value;
-    //         document.getElementById("failed").classList.add("failed");
+            document.getElementById("failed").style.display = "block"
+            document.getElementById("failed").innerHTML = value;
+            document.getElementById("failed").classList.add("failed");
 
-    //         setTimeout(() => {
-    //             document.getElementById("failed").style.display = "none"
-    //         }, 3500)
+            setTimeout(() => {
+                document.getElementById("failed").style.display = "none"
+            }, 3500)
 
-    //         button.disabled = false;
-    //       });
+            button.disabled = false;
+          });
 
-    //     button.style.backgroundColor = "#FF4500";
-    //     button.style.color = "white";
-    //     button.innerHTML = "Create Event";
-    // }
+        button.style.backgroundColor = "#FF4500";
+        button.style.color = "white";
+        button.innerHTML = "Create Event";
+    }
 
-    // } catch (error) {
-    //     document.getElementById("failed").style.display = "block"
-    //     document.getElementById("failed").innerHTML = "There Was An Error, Please Reload The Page";
-    //     document.getElementById("failed").classList.add("failed");
-    //     console.log(error);
-    // }
+    } catch (error) {
+        document.getElementById("failed").style.display = "block"
+        document.getElementById("failed").innerHTML = "There Was An Error, Please Reload The Page";
+        document.getElementById("failed").classList.add("failed");
+        console.log(error);
+    }
 });
 
 function sanitizeDescInput() {
