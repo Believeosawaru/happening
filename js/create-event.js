6e358d2d7eaa244eba48f41a5a85597cde02a6d4
believe <believeosawaru2@gmail.com>
2024-08-30 17:42:06 +0100

    vwhmv ef

--- js/create-event.js
+++ js/create-event.js
@@ -117,63 +117,63 @@ document.getElementById("create-event").addEventListener("submit", async (e) => 
         return console.log("Pick A Location");
     }
 
-    try {
-        const response = await fetch(signUpUrl, {
-            method: "POST",
-            headers: {
-                "Authorization": `Bearer ${token}`,
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                name,
-                description,
-                date,
-                time: dateInUTC.toISOString(),
-                timezone,
-                location,
-                type
-            })
-        });
+    // try {
+    //     const response = await fetch(signUpUrl, {
+    //         method: "POST",
+    //         headers: {
+    //             "Authorization": `Bearer ${token}`,
+    //             "Content-Type": "application/json"
+    //         },
+    //         body: JSON.stringify({
+    //             name,
+    //             description,
+    //             date,
+    //             time: dateInUTC.toISOString(),
+    //             timezone,
+    //             location,
+    //             type
+    //         })
+    //     });
 
-    const data = await response.json();
+    // const data = await response.json();
 
-    if (response.ok) {
-        document.getElementById("success-sign-up").innerHTML = "Event Created Successfully";
-        document.getElementById("success-sign-up").classList.add("success-sign-up");
+    // if (response.ok) {
+    //     document.getElementById("success-sign-up").innerHTML = "Event Created Successfully";
+    //     document.getElementById("success-sign-up").classList.add("success-sign-up");
 
-        button.style.cursor = "wait";
+    //     button.style.cursor = "wait";
 
-        setTimeout(() => {
-            window.location.href = "events.html"
-        }, 2000)
-    } else {
-        const keys = Object.keys(data);
+    //     setTimeout(() => {
+    //         window.location.href = "events.html"
+    //     }, 2000)
+    // } else {
+    //     const keys = Object.keys(data);
 
-        keys.forEach(key => {
-            const value = data[key]; 
+    //     keys.forEach(key => {
+    //         const value = data[key]; 
             
-            document.getElementById("failed").style.display = "block"
-            document.getElementById("failed").innerHTML = value;
-            document.getElementById("failed").classList.add("failed");
+    //         document.getElementById("failed").style.display = "block"
+    //         document.getElementById("failed").innerHTML = value;
+    //         document.getElementById("failed").classList.add("failed");
 
-            setTimeout(() => {
-                document.getElementById("failed").style.display = "none"
-            }, 3500)
+    //         setTimeout(() => {
+    //             document.getElementById("failed").style.display = "none"
+    //         }, 3500)
 
-            button.disabled = false;
-          });
+    //         button.disabled = false;
+    //       });
 
-        button.style.backgroundColor = "#FF4500";
-        button.style.color = "white";
-        button.innerHTML = "Create Event";
-    }
+    //     button.style.backgroundColor = "#FF4500";
+    //     button.style.color = "white";
+    //     button.innerHTML = "Create Event";
+    // }
 
-    } catch (error) {
-        document.getElementById("failed").style.display = "block"
-        document.getElementById("failed").innerHTML = "There Was An Error, Please Reload The Page";
-        document.getElementById("failed").classList.add("failed");
-        console.log(error);
-    }
+    // } catch (error) {
+    //     document.getElementById("failed").style.display = "block"
+    //     document.getElementById("failed").innerHTML = "There Was An Error, Please Reload The Page";
+    //     document.getElementById("failed").classList.add("failed");
+    //     console.log(error);
+    // }
 });
 
 function sanitizeDescInput() {
