d237896511b341679a850f3b6796d7eccaecfe44
believe <believeosawaru2@gmail.com>
2024-09-06 09:37:49 +0100

    9ijnokj mnrwuno kmj,

--- js/option.js
+++ js/option.js
@@ -8,4 +8,10 @@ if (groupId) {
 
         proceed.innerHTML = `<a href="invite-group.html?groupId=${groupId}">Proceed</a>`;
     }
+
+    function skip() {
+        const skipBtn = document.getElementById("skip-btn");
+
+        skipBtn.innerHTML = `<a href="groups.html" id="black">Skip For Later</a>`
+    }
 }
