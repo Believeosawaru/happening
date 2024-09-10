11564263890386024b58d1037b538d10b9585074
believe <believeosawaru2@gmail.com>
2024-09-10 14:27:30 +0100

    mn jnmjk

--- js/verification.js
+++ js/verification.js
@@ -10,7 +10,7 @@ function disableBtn() {
 
 const email = localStorage.getItem("email");
 
-console.log(email)
+console.log("This is The email" + email)
 
 document.getElementById("recover-password").addEventListener("submit", async (e) => {
     e.preventDefault();
