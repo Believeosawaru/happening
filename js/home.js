d19b82cb342f829605765d8e26ac58258dfe3068
believe <believeosawaru2@gmail.com>
2024-09-10 15:35:56 +0100

    jeh8gvuiy

--- js/home.js
+++ js/home.js
@@ -34,7 +34,7 @@ async function fetchMessage() {
         }
 
         if (!response.ok) {
-           console.log(response)
+           console.log(response.json());
         }
 
         const message = await response.json();
