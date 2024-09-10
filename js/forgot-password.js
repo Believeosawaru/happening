30c1ac41cd9ebe1692f9444c56dccf422cc47c4f
believe <believeosawaru2@gmail.com>
2024-09-10 14:02:23 +0100

    nibvyugnm hjybubj,

--- js/forgot-password.js
+++ js/forgot-password.js
@@ -1,4 +1,4 @@
-const signInUrl = "https://happening-api.onrender.com/api/v1/auth/forgot-password-code";
+const signInUrl = "http://5.161.186.15/api/v1/auth/forgot-password";
 
 const button = document.getElementById("fg-pass-btn");
 
