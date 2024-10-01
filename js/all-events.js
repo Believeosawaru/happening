3bb8af66a13e4fbffa6aa84509adf54d523201c7
believe <believeosawaru2@gmail.com>
2024-10-01 18:25:57 +0100

    kjhgvuyjnm m,njb hknb b

--- js/all-events.js
+++ js/all-events.js
@@ -1,4 +1,4 @@
-async function displayAllEvents() {
+async function displayPublicEvents() {
     const token = localStorage.getItem("authToken");
 
     if (!token) {
@@ -99,4 +99,106 @@ async function displayAllEvents() {
     }
 }
 
-window.onload = displayAllEvents;
+async function displayOnlineEvents() {
+    const token = localStorage.getItem("authToken");
+
+    if (!token) {
+        window.location.href = "/html/log-in.html"
+    }
+
+    try {
+        const response = await fetch("http://5.161.186.15/api/v1/user/all-events", {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        });
+
+        const data = await response.json();
+
+        if (data.message === "jwt malformed") {
+            setTimeout(() => {
+                window.location.href = "log-in.html"
+            }, 3500);
+        }
+
+        if (response.ok) {
+            const eventContainer = 
+            document.getElementById("online-events-list")
+
+            texts = "";
+
+            if (data.message.length < 1) {
+                return;
+            }
+
+            setTimeout(() => {
+                document.querySelector(".pre-loader").style.display = "none";
+            }, 3500);
+
+            data.message.forEach((event) => {
+            const eventDate = new Date(event.date);
+            const formattedDate = eventDate.toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric"
+            });
+
+                texts += `
+               <div>
+                       <h2>${event.name}</h2>
+                       <p class="dis-flex">
+                            <span class="material-symbols-outlined">
+                            location_on
+                            </span> 
+                            <span>${event.location || "None Specified"}</span>
+                        </p>
+
+                       <p class="dis-flex">
+                            <span class="material-symbols-outlined">
+                            schedule
+                            </span>
+                            <span>${event.time} ${event.timeZone}</span>
+                       </p>
+
+                        <p class="dis-flex">
+                            <span class="material-symbols-outlined">
+                                calendar_month
+                            </span>
+                            <span>${formattedDate}</span>
+                        </p>
+                    </div>
+
+                `
+            });
+
+            // <button><a href="https://happening-khaki.vercel.app/html/groups/join-group.html?groupId=${group._id}">Join Group</a></button>
+            // <p class="group-type">${group.groupType}</p>
+
+            eventContainer.innerHTML = texts;
+        } else {
+            const keys = Object.keys(data);
+    
+            keys.forEach(key => {
+                const value = data[key]; 
+                
+                document.getElementById("failed").style.display = "block"
+                document.getElementById("failed").innerHTML = value;
+                document.getElementById("failed").classList.add("failed");
+    
+                setTimeout(() => {
+                    document.getElementById("failed").style.display = "none"
+                }, 3500)
+              });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function events() {
+    displayPublicEvents();
+    displayOnlineEvents();
+}
+
+window.onload = events;
--- js/category.js
+++ js/category.js
@@ -1,8 +1,14 @@
-const catButton = document.getElementById("right");
+const publicCatButton = document.getElementById("right");
+const onlineCatButton = document.getElementById("right-2");
 
-catButton.addEventListener("click", () => {
+publicCatButton.addEventListener("click", () => {
     document.querySelector(".choose-category").classList.add("cate-transition");
     document.querySelector(".choose-category").classList.remove("close-cate");
+});
+
+onlineCatButton.addEventListener("click", () => {
+    document.querySelector("#choose-online").classList.add("cate-transition");
+    document.querySelector("#choose-online").classList.remove("close-cate");
 });
 
 document.getElementById("close").addEventListener("click", () => {
