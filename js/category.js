ac521734010f481bca20c946fb2758fc96bd5cec
believe <believeosawaru2@gmail.com>
2024-10-01 13:56:25 +0100

    nb jkbk h jhbu hj hj

--- js/category.js
+++ js/category.js
@@ -2,8 +2,10 @@ const catButton = document.getElementById("right");
 
 catButton.addEventListener("click", () => {
     document.querySelector(".choose-category").classList.add("cate-transition");
+    document.querySelector(".choose-category").classList.remove("close-cate");
 });
 
 function close() {
+    document.querySelector(".choose-category").classList.add("close-cate");
     document.querySelector(".choose-category").classList.remove("cate-transition");
 }
