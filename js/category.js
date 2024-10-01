8c70cf8eec471272a307f201649b7ec297bc4d4d
believe <believeosawaru2@gmail.com>
2024-10-01 12:45:48 +0100

    jnbljk jbblkm,

--- js/category.js
+++ js/category.js
@@ -1,5 +1,5 @@
 const catButton = document.getElementById("right");
 
 catButton.addEventListener("click", () => {
-    document.querySelector(".choose-category").style.display = "block";
+    document.querySelector(".choose-category").classList.add("cate-transition")
 });
