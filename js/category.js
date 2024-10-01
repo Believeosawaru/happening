a74e75712f81f8a67f9456dfb2dd01b369fae938
believe <believeosawaru2@gmail.com>
2024-10-01 13:52:28 +0100

    nb jkcfbkj dfbjknm

--- js/category.js
+++ js/category.js
@@ -2,8 +2,8 @@ const catButton = document.getElementById("right");
 
 catButton.addEventListener("click", () => {
     document.querySelector(".choose-category").classList.add("cate-transition");
-
-    // document.querySelector("body").addEventListener("click", () => {
-    //     document.querySelector(".choose-category").classList.remove("cate-transition")
-    // })
 });
+
+function close() {
+    document.querySelector(".choose-category").classList.remove("cate-transition");
+}
