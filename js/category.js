70e62f1e94ca60e87d6295526502a5014d8e92d2
believe <believeosawaru2@gmail.com>
2024-10-01 13:16:57 +0100

    nik jvhbkjn mf

--- js/category.js
+++ js/category.js
@@ -3,3 +3,7 @@ const catButton = document.getElementById("right");
 catButton.addEventListener("click", () => {
     document.querySelector(".choose-category").classList.add("cate-transition")
 });
+
+document.querySelector("body").addEventListener("click", () => {
+    document.querySelector(".choose-category").classList.remove("cate-transition")
+})
