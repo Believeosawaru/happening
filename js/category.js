4a777a9505301d4a6c7790f8c4f6427df478cc3a
believe <believeosawaru2@gmail.com>
2024-10-01 13:17:57 +0100

    nikj km,

--- js/category.js
+++ js/category.js
@@ -1,9 +1,9 @@
 const catButton = document.getElementById("right");
 
 catButton.addEventListener("click", () => {
-    document.querySelector(".choose-category").classList.add("cate-transition")
-});
+    document.querySelector(".choose-category").classList.add("cate-transition");
 
-document.querySelector("body").addEventListener("click", () => {
-    document.querySelector(".choose-category").classList.remove("cate-transition")
-})
+    document.querySelector("body").addEventListener("click", () => {
+        document.querySelector(".choose-category").classList.remove("cate-transition")
+    })
+});
