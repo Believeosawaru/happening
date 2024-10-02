e57300a3f7cc209e7c6994b8e7afa18b62c7e159
believe <believeosawaru2@gmail.com>
2024-10-02 19:47:24 +0100

    nb hbuikjbikhnj

--- js/event-details.js
+++ js/event-details.js
@@ -39,47 +39,51 @@ document.addEventListener("DOMContentLoaded", async () => {
                 });
                 
                 if (result.createdBy._id !== result.currentUserId) {
-                    eventDetailsCon.innerHTML = `
-                  <h2 id="gd-h2 bttm-space">${result.data.name} <span id="h2-p">${result.data.type}</span></h2>
-
-                <p>
-                 ${result.data.description}
-                </p>
-
-                <p class="dis-flex event-date">
-                 <span class="material-symbols-outlined">
-                    calendar_month
-                 </span>
-                 <span>${formattedDate}</span>
-                </p>
-
-                <p class="dis-flex">
-                <span class="material-symbols-outlined">
-                 schedule
-                </span>
-
-                <span>
-                 ${result.data.time} (${result.data.timeZone} UTC)
-                </span>
-                </p>
-
-                <p class="dis-flex">
+                    eventDetailsCon.innerHTML = `                <div id="group-details-container">
+                    <h2 id="gd-h2 bttm-space">
+                        ${result.data.name} <span id="h2-p">${result.data.type}</span>
+                    </h2>
+    
+                    <p class="dis-flex event-date">
+                     <span class="material-symbols-outlined">
+                        calendar_month
+                     </span>
+                     <span>${formattedDate}</span>
+                    </p>
+    
+                    <div id="desc">
+                        <p class="dis-flex">
                             <span class="material-symbols-outlined">
-                            location_on
-                            </span> 
-                            <span>${result.data.location || "None Specified"}</span>
-                        </p>
-                <p>
-
-                <p>Organizer: <br>
-                 <p class="dis-flex">
-                    <span class="material-symbols-outlined">
-                    person
-                    </span>
-                    <span>${result.createdBy.firstName} ${result.createdBy.lastName}</span>
-                 </p>
-                </p>
-                `;
+                             schedule
+                            </span>
+            
+                            <span>
+                             ${result.data.time} (${result.data.timeZone} UTC)
+                            </span>
+                            </p>
+            
+                            <p class="dis-flex">
+                                    <span class="material-symbols-outlined">
+                                    location_on
+                                    </span> 
+                                    <span>${result.data.location || "None Specified"}</span>
+                            </p>
+                    </div>
+    
+                     <p class="dis-flex">
+                        <span class="material-symbols-outlined">
+                        person
+                        </span>
+                        <span>${result.createdBy.firstName} ${result.createdBy.lastName} <span id="h2-p">Organizer</span></span>
+                     </p>
+                </div>
+    
+                <div class="event-info">
+                    <h3>About This Event</h3>
+                    <p>
+                        ${result.data.description}
+                    </p>   
+                </div>`;
                 } else {
                     eventDetailsCon.innerHTML = `
 
