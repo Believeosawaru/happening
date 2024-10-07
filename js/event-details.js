64238c9ef5a4c36bb5084f86895e30eb697cc4fd
believe <believeosawaru2@gmail.com>
2024-10-07 13:13:07 +0100

    jnhubhk bhj

--- js/event-details.js
+++ js/event-details.js
@@ -87,26 +87,52 @@ document.addEventListener("DOMContentLoaded", async () => {
                 </div>`;
                 } else {
                     eventDetailsCon.innerHTML = `
-
-                    <h2 id="gd-h2 bttm-space">${result.data.name} <span id="h2-p">${result.data.type}</span></h2>
-
-                    <p class="event-date">
-                         &#x1F4C5; ${formattedDate}
-                    </p>
+                    <div id="group-details-container">
+                    <h1 id="gd-h2 bttm-space">
+                        ${result.data.name} <span id="h2-p">${result.data.type}</span>
+                    </h1>
     
                     <div id="desc">
-                        <p><span>Event Description:</span> ${result.data.description}</p>
-    
-                        <p><span>Location:</span> ${result.data.location}</p>
-    
-                        <p><span>Time:</span> &#x23F0; ${result.data.time}</p>
-    
-                        <button><a href="event-iv-send.html?eventId=${eventId}">Send Invitation</a></button>
+                    <p class="dis-flex event-date">
+                     <span class="material-symbols-outlined">
+                        calendar_month
+                     </span>
+                     <span>${formattedDate}</span>
+                    </p>
+
+                    <p class="dis-flex">
+                        <span class="material-symbols-outlined">
+                            schedule
+                        </span>
+        
+                        <span>
+                            ${result.data.time} (${result.data.timeZone} UTC)
+                        </span>
+                    </p>
+            
+                    <p class="dis-flex">
+                        <span class="material-symbols-outlined">
+                        location_on
+                        </span> 
+                        <span>${result.data.location || "None Specified"}</span>
+                    </p>
+
+                     <p class="dis-flex">
+                        <span class="material-symbols-outlined">
+                        person
+                        </span>
+                        <span>${result.createdBy.firstName} ${result.createdBy.lastName} <span id="h2-p">Organizer</span></span>
+                     </p>
                     </div>
+                </div>
     
-                     
-                    <span class="edit-button"><a href="event-details-edit.html?eventId=${eventId}"><i class="fa fa-pencil"></i></a></span>    
-                `;
+                <div class="event-info">
+                    <h3>About This Event</h3>
+                    <p>
+                        As the autumn leaves begin to fall, Sarah and Jake are excitedly preparing for their upcoming marriage. Surrounded by family and friends, they’ll exchange vows in a charming countryside venue, filled with vibrant flowers and soft, golden light. Their love story, blossomed over years of laughter and adventures, will culminate in a celebration of commitment and joy. With heartfelt music, delicious food, and personal touches that reflect their journey together, the day promises to be unforgettable. As they embark on this new chapter, they look forward to a future filled with love, growth, and shared dreams.
+                    </p>   
+                    <button>Send Email Invitation</button>
+                </div>`;
                 }
 
             } else {
