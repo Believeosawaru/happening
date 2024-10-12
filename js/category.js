2af8b4b44ebbd35b9542739234d78327bdf14652
believe <believeosawaru2@gmail.com>
2024-10-12 13:06:47 +0100

    huivfhjbhkjfm

--- js/category.js
+++ js/category.js
@@ -1,14 +1,29 @@
 const publicCatButton = document.getElementById("right");
 const onlineCatButton = document.querySelector(".right-2");
+const categories = [
+    "Music",
+    "Business",
+    "Sports",
+    "Arts",
+    "Food",
+    "Education",
+    "Kids",
+    "Technology",
+    "Health",
+    "Fashion",
+    "Film",
+    "Nightlife",
+    "Literature",
+    "Travel",
+    "Nature",
+    "Charity",
+    "Wellness",
+    "Culture"
+];
 
 publicCatButton.addEventListener("click", () => {
     document.querySelector(".choose-category").classList.add("cate-transition");
     document.querySelector(".choose-category").classList.remove("close-cate");
-});
-
-onlineCatButton.addEventListener("click", () => {
-    document.querySelector("#choose-online").classList.add("cate-transition");
-    document.querySelector("#choose-online").classList.remove("close-cate");
 });
 
 document.getElementById("close").addEventListener("click", () => {
@@ -16,7 +31,12 @@ document.getElementById("close").addEventListener("click", () => {
     document.querySelector(".choose-category").classList.remove("cate-transition");
 });
 
-document.querySelector(".close-2").addEventListener("click", () => {
-    document.querySelector("#choose-online").classList.add("close-cate");
-    document.querySelector("#choose-online").classList.remove("cate-transition");
+const select = document.getElementById("category");
+
+categories.forEach((category) => {
+    const option = document.createElement("option");
+    option.value = category;
+    option.text = category;
+
+    select.appendChild(option);
 });
