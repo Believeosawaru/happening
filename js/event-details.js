af16ff400412909318d6faefe322e3d1612618c7
believe <believeosawaru2@gmail.com>
2024-10-21 13:06:07 +0100

    dhvbkh jf nm

--- js/event-details.js
+++ js/event-details.js
@@ -79,7 +79,7 @@ document.addEventListener("DOMContentLoaded", async () => {
                         <span class="material-symbols-outlined">
                         person
                         </span>
-                        <span>${result.createdBy.firstName} ${result.createdBy.lastName} <span id="h2-p">Organizer</span></span>
+                        <span><a href="user-profile.html/${result.createdBy._id}">${result.createdBy.firstName} ${result.createdBy.lastName} </a><span id="h2-p">Organizer</span></span>
                      </p>
                     </div>
                 </div>
