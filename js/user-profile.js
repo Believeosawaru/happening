d5d4222a14bb80156704b5ef97eb9bf6c37bcb55
believe <believeosawaru2@gmail.com>
2024-10-21 15:56:05 +0100

    ubnfjofkj

--- js/user-profile.js
+++ js/user-profile.js
@@ -14,12 +14,14 @@
 //         menu.classList.remove('open-menu');
 //     }
 // }
+const urlParams = new URLSearchParams(window.location.search);
+const userId = urlParams.get("userId");
 
 async function userProfile() {
     const token = localStorage.getItem("authToken");
 
     try {
-        const response = await fetch("http://5.161.186.15/api/v1/user/user-profile", {
+        const response = await fetch(`http://5.161.186.15/api/v1/user/user-profile/${userId}`, {
             method: "GET",
             headers: {
                 "Authorization": `Bearer ${token}`
