3b92cb573f8795f999cf567d645952e762dd09bd
believe <believeosawaru2@gmail.com>
2024-10-21 16:06:27 +0100

    monk jj

--- js/user-profile.js
+++ js/user-profile.js
@@ -41,7 +41,7 @@ async function userProfile() {
                 document.querySelector(".pre-loader").style.display = "none";
             }, 3500);
 
-            document.getElementById("user-name").innerHTML = `${message.message}`;
+            document.getElementById("user-name").innerHTML = `${message.data.firstName} ${message.data.lastName}`;
 
             console.log(message)
 
