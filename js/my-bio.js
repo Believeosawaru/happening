2ccc8d6af1ff11c8d2fd50fcc782709399cfd25c
believe <believeosawaru2@gmail.com>
2024-10-22 14:58:23 +0100

    gbuik jm,

--- js/my-bio.js
+++ js/my-bio.js
@@ -33,4 +33,74 @@ async function myBio() {
     }
 }
 
+function disableBtn() {
+    const button = document.getElementById("bio");
+    button.disabled = true;
+    button.style.backgroundColor = "#FFECB3";
+    button.style.color = "black";
+    button.innerHTML = "Submitting...."
+}
+
+const bioUrl = "http://5.161.186.15/api/v1/user/my-bio";
+
+document.getElementById("create-event").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const bio = document.getElementById("bio").value;
+
+    disableBtn();
+
+    try {
+        const response = await fetch(bioUrl, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+               bio
+            })
+        });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        document.getElementById("success-sign-up").innerHTML = "Bio Updated Successfully";
+        document.getElementById("success-sign-up").classList.add("success-sign-up");
+
+        button.style.cursor = "wait";
+
+        setTimeout(() => {
+            window.location.href = "my-profile.html"
+        }, 2000)
+    } else {
+        const keys = Object.keys(data);
+
+        keys.forEach(key => {
+            const value = data[key]; 
+            
+            document.getElementById("failed").style.display = "block"
+            document.getElementById("failed").innerHTML = value;
+            document.getElementById("failed").classList.add("failed");
+
+            setTimeout(() => {
+                document.getElementById("failed").style.display = "none"
+            }, 3500)
+
+            button.disabled = false;
+          });
+
+        button.style.backgroundColor = "#FF4500";
+        button.style.color = "white";
+        button.innerHTML = "Submit";
+    }
+
+    } catch (error) {
+        document.getElementById("failed").style.display = "block"
+        document.getElementById("failed").innerHTML = "There Was An Error, Please Reload The Page";
+        document.getElementById("failed").classList.add("failed");
+        console.log(error);
+    }
+});
+
 window.onload = myBio;
