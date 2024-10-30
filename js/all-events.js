ccf9423a437c91c482061b50a443b6e84bade487
believe <believeosawaru2@gmail.com>
2024-10-30 06:51:58 +0100

    jipnklm,

--- js/all-events.js
+++ js/all-events.js
@@ -91,106 +91,106 @@ async function displayPublicEvents() {
     }
 }
 
-async function displayOnlineEvents() {
-    const token = localStorage.getItem("authToken");
+// async function displayOnlineEvents() {
+//     const token = localStorage.getItem("authToken");
 
-    if (!token) {
-        window.location.href = "/html/log-in.html"
-    }
+//     if (!token) {
+//         window.location.href = "/html/log-in.html"
+//     }
 
-    try {
-        const response = await fetch("http://5.161.186.15/api/v1/user/all-events", {
-            method: "GET",
-            headers: {
-                "Authorization": `Bearer ${token}`
-            }
-        });
+//     try {
+//         const response = await fetch("http://5.161.186.15/api/v1/user/all-events", {
+//             method: "GET",
+//             headers: {
+//                 "Authorization": `Bearer ${token}`
+//             }
+//         });
 
-        const data = await response.json();
+//         const data = await response.json();
 
-        if (data.message === "jwt malformed" || data.message === "jwt malformed") {
-            setTimeout(() => {
-                window.location.href = "log-in.html"
-            }, 3500);
-        }
+//         if (data.message === "jwt malformed" || data.message === "jwt malformed") {
+//             setTimeout(() => {
+//                 window.location.href = "log-in.html"
+//             }, 3500);
+//         }
 
-        if (response.ok) {
-            const eventContainer = 
-            document.getElementById("online-events-list")
+//         if (response.ok) {
+//             const eventContainer = 
+//             document.getElementById("online-events-list")
 
-            texts = "";
+//             texts = "";
 
-            if (data.message.length < 1) {
-                return;
-            }
+//             if (data.message.length < 1) {
+//                 return;
+//             }
 
-            setTimeout(() => {
-                document.querySelector(".pre-loader").style.display = "none";
-            }, 3500);
+//             setTimeout(() => {
+//                 document.querySelector(".pre-loader").style.display = "none";
+//             }, 3500);
 
-            data.message.forEach((event) => {
-            const eventDate = new Date(event.date);
-            const formattedDate = eventDate.toLocaleDateString("en-US", {
-                year: "numeric",
-                month: "long",
-                day: "numeric"
-            });
+//             data.message.forEach((event) => {
+//             const eventDate = new Date(event.date);
+//             const formattedDate = eventDate.toLocaleDateString("en-US", {
+//                 year: "numeric",
+//                 month: "long",
+//                 day: "numeric"
+//             });
 
-                texts += `
-               <div>
-                       <h2>${event.name}</h2>
-                       <p class="dis-flex">
-                            <span class="material-symbols-outlined">
-                            location_on
-                            </span> 
-                            <span>${event.location || "None Specified"}</span>
-                        </p>
+//                 texts += `
+//                <div>
+//                        <h2>${event.name}</h2>
+//                        <p class="dis-flex">
+//                             <span class="material-symbols-outlined">
+//                             location_on
+//                             </span> 
+//                             <span>${event.location || "None Specified"}</span>
+//                         </p>
 
-                       <p class="dis-flex">
-                            <span class="material-symbols-outlined">
-                            schedule
-                            </span>
-                            <span>${event.time} ${event.timeZone}</span>
-                       </p>
+//                        <p class="dis-flex">
+//                             <span class="material-symbols-outlined">
+//                             schedule
+//                             </span>
+//                             <span>${event.time} ${event.timeZone}</span>
+//                        </p>
 
-                        <p class="dis-flex">
-                            <span class="material-symbols-outlined">
-                                calendar_month
-                            </span>
-                            <span>${formattedDate}</span>
-                        </p>
-                    </div>
+//                         <p class="dis-flex">
+//                             <span class="material-symbols-outlined">
+//                                 calendar_month
+//                             </span>
+//                             <span>${formattedDate}</span>
+//                         </p>
+//                     </div>
 
-                `
-            });
+//                 `
+//             });
 
-            // <button><a href="https://happening-khaki.vercel.app/html/groups/join-group.html?groupId=${group._id}">Join Group</a></button>
-            // <p class="group-type">${group.groupType}</p>
+//             // <button><a href="https://happening-khaki.vercel.app/html/groups/join-group.html?groupId=${group._id}">Join Group</a></button>
+//             // <p class="group-type">${group.groupType}</p>
 
-            eventContainer.innerHTML = texts;
-        } else {
-            const keys = Object.keys(data);
+//             eventContainer.innerHTML = texts;
+//         } else {
+//             const keys = Object.keys(data);
     
-            keys.forEach(key => {
-                const value = data[key]; 
+//             keys.forEach(key => {
+//                 const value = data[key]; 
                 
-                document.getElementById("failed").style.display = "block"
-                document.getElementById("failed").innerHTML = value;
-                document.getElementById("failed").classList.add("failed");
+//                 document.getElementById("failed").style.display = "block"
+//                 document.getElementById("failed").innerHTML = value;
+//                 document.getElementById("failed").classList.add("failed");
     
-                setTimeout(() => {
-                    document.getElementById("failed").style.display = "none"
-                }, 3500)
-              });
-        }
-    } catch (error) {
-        console.log(error);
-    }
-}
+//                 setTimeout(() => {
+//                     document.getElementById("failed").style.display = "none"
+//                 }, 3500)
+//               });
+//         }
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
 
 function events() {
     displayPublicEvents();
-    displayOnlineEvents();
+    // displayOnlineEvents();
 }
 
 window.onload = events;
