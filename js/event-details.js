8893ea58320f5f2f4cce03dd78a93657192a94a0
believe <believeosawaru2@gmail.com>
2024-11-01 21:06:26 +0100

    b hkjnb

--- js/event-details.js
+++ js/event-details.js
@@ -143,8 +143,8 @@ document.addEventListener("DOMContentLoaded", async () => {
                         <span>${result.createdBy.firstName} ${result.createdBy.lastName} <span id="h2-p">Organizer</span></span>
                      </p>
 
-                     <a href="event-details-edit.html?eventId=${result._id}" id="edit-event">
-                        <span class="material-symbols-outlined" id="edit-event">
+                     <span class="material-symbols-outlined" id="edit-event">
+                     <a href="event-details-edit.html?eventId=${result._id}" id="edit-event-pen">
                             edit
                         </span>
                      </a>
