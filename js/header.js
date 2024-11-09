1eedf8745972c9159b97112e7cda3706b08e5898
believe <believeosawaru2@gmail.com>
2024-11-09 15:57:35 +0100

    nbfjkfj fkm,

--- js/header.js
+++ js/header.js
@@ -13,57 +13,57 @@ function openMenu() {
 }
 
 document.getElementById("bell").addEventListener("click", () => {
-    window.location.href = "http://5.161.186.15/notifications.html"
+    window.location.href = "http://5.161.186.15/notifications"
 })
 
 document.querySelector(".user-menu").innerHTML = `
-            <p><a class="dis-flex" href="http://5.161.186.15/home.html"><span class="material-symbols-outlined">
+            <p><a class="dis-flex" href="http://5.161.186.15/home"><span class="material-symbols-outlined">
                 home
                 </span> <span>Home</span></a></p>
                 
-            <p><a class="dis-flex" href="http://5.161.186.15/events/events.html"><span class="material-symbols-outlined">
+            <p><a class="dis-flex" href="http://5.161.186.15/events/events"><span class="material-symbols-outlined">
                 event_available
                 </span> <span>Events</span></a></p>
 
-            <p><a class="dis-flex" href="http://5.161.186.15/groups/groups.html"><span class="material-symbols-outlined">
+            <p><a class="dis-flex" href="http://5.161.186.15/groups"><span class="material-symbols-outlined">
                 groups
                 </span> <span>Groups</span></a></p>
 
-            <p><a class="dis-flex" href="http://5.161.186.15/settings.html"><span class="material-symbols-outlined">
+            <p><a class="dis-flex" href="http://5.161.186.15/settings"><span class="material-symbols-outlined">
                 settings
                 </span> <span>Settings</span></a></p>
 
-            <p><a class="dis-flex" href="http://5.161.186.15/tos.html"><span class="material-symbols-outlined">
+            <p><a class="dis-flex" href="http://5.161.186.15/tos"><span class="material-symbols-outlined">
                 description
                 </span> <span>Terms Of Service</span></a></p>
 
-            <p><a class="dis-flex" href="http://5.161.186.15/my-profile.html"><span class="material-symbols-outlined">
+            <p><a class="dis-flex" href="http://5.161.186.15/my-profile"><span class="material-symbols-outlined">
                 person
                 </span> <span>My Profile</span></a></p>
-            <button class="first-btn"><a href="http://5.161.186.15/log-out.html">Log Out</a></button>
+            <button class="first-btn"><a href="http://5.161.186.15/log-out">Log Out</a></button>
 `;
 
 document.querySelector(".pc-menu").innerHTML = ` 
-                <a href="http://5.161.186.15/home.html"><span class="material-symbols-outlined">
+                <a href="http://5.161.186.15/home"><span class="material-symbols-outlined">
                     home
                     </span> Home</a>
                     
-                <a href="http://5.161.186.15/events/events.html"><span class="material-symbols-outlined">
+                <a href="http://5.161.186.15/events/events"><span class="material-symbols-outlined">
                     event_available
                     </span>  Events</a>
                     
-                <a href="http://5.161.186.15/groups/groups.html"><span class="material-symbols-outlined">
+                <a href="http://5.161.186.15/groups/groups"><span class="material-symbols-outlined">
                     groups
                     </span> Groups</a>
                     
-                <a href="http://5.161.186.15/settings.html"><span class="material-symbols-outlined">
+                <a href="http://5.161.186.15/settings"><span class="material-symbols-outlined">
                     settings
                     </span> Settings</a>
                     
-                <a href="http://5.161.186.15/my-profile.html"><span class="material-symbols-outlined">
+                <a href="http://5.161.186.15/my-profile"><span class="material-symbols-outlined">
                     person
                     </span> My Profile</a>
-                <button class="first-btn"><a href="http://5.161.186.15/log-out.html">Log Out</a></button>
+                <button class="first-btn"><a href="http://5.161.186.15/log-out">Log Out</a></button>
 `;
 
 const userToken = localStorage.getItem("authToken");
@@ -81,7 +81,7 @@ async function loadNotifications() {
 
         if (data.message === "jwt malformed" || data.message === "jwt expired") {
             setTimeout(() => {
-                window.location.href = "log-in.html"
+                window.location.href = "log-in"
             }, 350);
         }
 
