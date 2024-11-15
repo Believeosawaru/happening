09e009ba7bb8d11121c2aa943a0426dc067d41cd
believe <believeosawaru2@gmail.com>
2024-11-15 09:26:48 +0100

    nikl m,nf

--- js/header.js
+++ js/header.js
@@ -13,47 +13,47 @@ function openMenu() {
 }
 
 document.getElementById("bell").addEventListener("click", () => {
-    window.location.href = "http://5.161.186.15/notifications"
+    window.location.href = "https://happening.net/notifications"
 })
 
 document.querySelector(".user-menu").innerHTML = `
-            <p><a class="dis-flex" href="http://5.161.186.15/home"><span class="material-symbols-outlined">
+            <p><a class="dis-flex" href="https://happening.net/home"><span class="material-symbols-outlined">
                 home
                 </span> <span>Home</span></a></p>
                 
-            <p><a class="dis-flex" href="http://5.161.186.15/my-events"><span class="material-symbols-outlined">
+            <p><a class="dis-flex" href="https://happening.net/my-events"><span class="material-symbols-outlined">
                 event_available
                 </span> <span>Events</span></a></p>
 
-            <p><a class="dis-flex" href="http://5.161.186.15/my-groups"><span class="material-symbols-outlined">
+            <p><a class="dis-flex" href="https://happening.net/my-groups"><span class="material-symbols-outlined">
                 groups
                 </span> <span>Groups</span></a></p>
 
-            <p><a class="dis-flex" href="http://5.161.186.15/settings"><span class="material-symbols-outlined">
+            <p><a class="dis-flex" href="https://happening.net/settings"><span class="material-symbols-outlined">
                 settings
                 </span> <span>Settings</span></a></p>
 
-            <p><a class="dis-flex" href="http://5.161.186.15/tos"><span class="material-symbols-outlined">
+            <p><a class="dis-flex" href="https://happening.net/tos"><span class="material-symbols-outlined">
                 description
                 </span> <span>Terms Of Service</span></a></p>
 
-            <p><a class="dis-flex" href="http://5.161.186.15/my-profile"><span class="material-symbols-outlined">
+            <p><a class="dis-flex" href="https://happening.net/my-profile"><span class="material-symbols-outlined">
                 person
                 </span> <span>My Profile</span></a></p>
-            <button class="first-btn"><a href="http://5.161.186.15/log-out">Log Out</a></button>
+            <button class="first-btn"><a href="https://happening.net/log-out">Log Out</a></button>
 `;
 
 document.querySelector("nav").innerHTML = `
-        <span><a href="http://5.161.186.15/home.html">Happening</a></span>
+        <span><a href="https://happening.net/home">Happening</a></span>
 
             <div class="pc-menu home-pc-menu">
-                <a href="http://5.161.186.15/home">&#x1F3E0; Home</a>
-                <a href="http://5.161.186.15/my-events">&#x1F4C5; Events</a>
-                <a href="http://5.161.186.15/my-groups">&#x1F465; Groups</a>
-                <a href="http://5.161.186.15/settings">&#x2699; Settings</a>
-                <a href="http://5.161.186.15/tos">&#x1F4DC; Terms Of Service</a>
+                <a href="https://happening.net/home">&#x1F3E0; Home</a>
+                <a href="https://happening.net/my-events">&#x1F4C5; Events</a>
+                <a href="https://happening.net/my-groups">&#x1F465; Groups</a>
+                <a href="https://happening.net/settings">&#x2699; Settings</a>
+                <a href="https://happening.net/tos">&#x1F4DC; Terms Of Service</a>
                 
-                <button class="first-btn"><a href="http://5.161.186.15/log-out">Log Out</a></button>
+                <button class="first-btn"><a href="https://happening.net/log-out">Log Out</a></button>
             </div>
 
             <i class="fa fa-bell" id="bell"><i class="alert"></i></i>
@@ -65,7 +65,7 @@ const userToken = localStorage.getItem("authToken");
 
 async function loadNotifications() {
     try {
-        const response = await fetch(`http://5.161.186.15/api/v1/user/my-notifications`, {
+        const response = await fetch(`https://happening.net/api/v1/user/my-notifications`, {
             method: "GET",
             headers: {
                 "Authorization": `Bearer ${userToken}`
@@ -76,7 +76,7 @@ async function loadNotifications() {
 
         if (data.message === "jwt malformed" || data.message === "jwt expired") {
             setTimeout(() => {
-                window.location.href = "http://5.161.186.15/log-in"
+                window.location.href = "https://happening.net/log-in"
             }, 350);
         }
 
