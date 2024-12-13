7cea8de2bff098f690326dd0a9079278bbec3942
believe <believeosawaru2@gmail.com>
2024-12-13 12:58:04 +0100

    fnhfbjk

---
+++ js/create-blog-post.js
@@ -0,0 +1,72 @@
+const token = localStorage.getItem("authToken");
+
+const button = document.getElementById("bio-btn");
+
+function disableBtn() {
+    button.disabled = true;
+    button.style.backgroundColor = "#FFECB3";
+    button.style.color = "black";
+    button.innerHTML = "Submitting...."
+}
+
+const postUrl = "https://happening.net/api/v1/blog/create-post";
+
+document.getElementById("create-post").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const post = document.getElementById("post").value;
+
+    disableBtn();
+
+    try {
+        const response = await fetch(postUrl, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+               content: post
+            })
+        });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        document.getElementById("success-sign-up").innerHTML = "Update Posted Successfully";
+        document.getElementById("success-sign-up").classList.add("success-sign-up");
+
+        button.style.cursor = "wait";
+
+        setTimeout(() => {
+            window.location.href = "https://happening.net/home"
+        }, 2000)
+    } else {
+        const keys = Object.keys(data);
+
+        keys.forEach(key => {
+            const value = data[key]; 
+            
+            document.getElementById("failed").style.display = "block"
+            document.getElementById("failed").innerHTML = value;
+            document.getElementById("failed").classList.add("failed");
+
+            setTimeout(() => {
+                document.getElementById("failed").style.display = "none"
+            }, 3500)
+
+            button.disabled = false;
+          });
+
+        button.style.backgroundColor = "#FF4500";
+        button.style.color = "white";
+        button.innerHTML = "Submit";
+    }
+
+    } catch (error) {
+        document.getElementById("failed").style.display = "block"
+        document.getElementById("failed").innerHTML = "There Was An Error, Please Reload The Page";
+        document.getElementById("failed").classList.add("failed");
+        console.log(error);
+    }
+});
