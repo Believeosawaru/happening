5364e6b718d6def57bdb244dc1f3162a38231973
believe <believeosawaru2@gmail.com>
2024-12-17 12:49:04 +0100

    fnbif hjknm

--- js/blog-options.js
+++ js/blog-options.js
@@ -3,7 +3,7 @@ const postId = urlParams.get("id");
 
 document.getElementById("blog-options").innerHTML =`
         <div class="center">
-            <button><a href="https://happening.net/blogs/edit-blog-post?id=${postId}">Edit Post</a></button>
+            <button type="button"><a href="https://happening.net/blogs/edit-blog-post?id=${postId}">Edit Post</a></button>
         </div>
-        <button id="delete-event-btn"><a style="color: #FF4500" href="https://happening.net/blogs/delete-blog-post?id=${postId}">Delete Post</a></button>`;
+        <button type="button" id="delete-event-btn"><a style="color: #FF4500" href="https://happening.net/blogs/delete-blog-post?id=${postId}">Delete Post</a></button>`;
 
