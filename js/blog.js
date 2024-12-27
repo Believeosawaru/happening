c83cc83fe827289d8b845329a07e8b817a0e6134
believe <believeosawaru2@gmail.com>
2024-12-27 14:29:11 +0100

    refactor: simplify truncateContent function by removing unnecessary split operation

--- js/blog.js
+++ js/blog.js
@@ -26,7 +26,7 @@ async function blog() {
             }
 
             function truncateContent(post, wordLimit = 50) {
-                const words = post.content.split("");
+                const words = post.content;
                 if (words.length > wordLimit) {
                     const truncated = words.slice(0, wordLimit).join(" ");
                     return `${truncated}... <a href="fullpost.html?postId=${post.id}">Read more</a>`;
