90f9e71dae1d4371587be9129a5fb457b5ad135d
believe <believeosawaru2@gmail.com>
2024-12-27 20:16:41 +0100

    nbjk m,

--- js/blog-options.js
+++
@@ -1,9 +0,0 @@
-const urlParams = new URLSearchParams(window.location.search);
-const postId = urlParams.get("id");
-
-document.getElementById("blog-options").innerHTML =`
-        <div class="center">
-            <button type="button"><a href="https://happening.net/blogs/edit-blog-post?id=${postId}">Edit Post</a></button>
-        </div>
-        <button type="button" id="delete-event-btn"><a style="color: #FF4500" href="https://happening.net/blogs/delete-blog-post?id=${postId}">Delete Post</a></button>`;
-
--- js/blog-post.js
+++ js/blog-post.js
@@ -3,48 +3,98 @@ const urlParams = new URLSearchParams(window.location.search);
 const postId = urlParams.get("postId");
 
 async function blog() {
-    try {
-const response = await fetch(`https://happening.net/api/v1/blog/blog-post/${postId}`, {
-            method: "GET"
-        });
-
-        const post = await response.json();
-
-        if (post.message === "jwt malformed" || post.message === "jwt expired") {
-            setTimeout(() => {
-                window.location.href = "https://happening.net/log-in"
-            }, 350);
-        }
-
-        if (response.ok) {
-            const postDate = new Date(post.date);
-                const formattedDate = postDate.toLocaleDateString("en-US", {
-                    year: "numeric",
-                    month: "long",
-                    day: "numeric"
-                });
-
-            document.getElementById("my-feed").innerHTML = `
-            <div id="post-card">
-                <div id="user-details">
-                    <img src="../../images/event.jpg" alt="User Image">
-                    <section>
-                        <h3>${post.author.firstName} ${post.author.lastName}</h3>
-                        <span>${formattedDate}</span>
-                    </section>
-                </div>
-                     <p>${post.content}</p>
-                    ${
-                        post.mediaPath && post.mediaType ? `<div id="flexy">
-                        ${mediaHTML}
-                    </div>` : ""
+    if (!token) {
+        try {
+            const response = await fetch(`https://happening.net/api/v1/blog/public-blog-post/${postId}`, {
+                        method: "GET"
+                    });
+            
+                    const post = await response.json();
+            
+                    if (post.message === "jwt malformed" || post.message === "jwt expired") {
+                        setTimeout(() => {
+                            window.location.href = "https://happening.net/log-in"
+                        }, 350);
                     }
-            </div>`
-         } else {
-            console.log(message)
-        }
-    } catch (error) {
-        console.log(error);
+            
+                    if (response.ok) {
+                        const postDate = new Date(post.date);
+                            const formattedDate = postDate.toLocaleDateString("en-US", {
+                                year: "numeric",
+                                month: "long",
+                                day: "numeric"
+                            });
+            
+                        document.getElementById("my-feed").innerHTML = `
+                        <div id="post-card">
+                            <div id="user-details">
+                                <img src="../../images/event.jpg" alt="User Image">
+                                <section>
+                                    <h3>${post.author.firstName} ${post.author.lastName}</h3>
+                                    <span>${formattedDate}</span>
+                                </section>
+                            </div>
+                                 <p>${post.content}</p>
+                                ${
+                                    post.mediaPath && post.mediaType ? `<div id="flexy">
+                                    ${mediaHTML}
+                                </div>` : ""
+                                }
+                        </div>`
+                     } else {
+                        console.log(message)
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+    } else {
+        try {
+            const response = await fetch(`https://happening.net/api/v1/blog/blog-post/${postId}`, {
+                        method: "GET", 
+                        headers: {
+                            "Authorization": `Bearer ${token}`
+                        }
+                    });
+            
+                    const post = await response.json();
+            
+                    if (post.message === "jwt malformed" || post.message === "jwt expired") {
+                        setTimeout(() => {
+                            window.location.href = "https://happening.net/log-in"
+                        }, 350);
+                    }
+            
+                    if (response.ok) {
+                        const postDate = new Date(post.date);
+                            const formattedDate = postDate.toLocaleDateString("en-US", {
+                                year: "numeric",
+                                month: "long",
+                                day: "numeric"
+                            });
+            
+                        document.getElementById("my-feed").innerHTML = `
+                        <div id="post-card">
+                            <div id="user-details">
+                                <img src="../../images/event.jpg" alt="User Image">
+                                <section>
+                                    <h3>${post.author.firstName} ${post.author.lastName}</h3>
+                                    <span>${formattedDate}</span>
+                                </section>
+                            </div>
+                                 <p>${post.content}</p>
+                                ${
+                                    post.mediaPath && post.mediaType ? `<div id="flexy">
+                                    ${mediaHTML}
+                                </div>` : ""
+                                }
+                        </div>
+                        `
+                     } else {
+                        console.log(message)
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
     }
 }
 
