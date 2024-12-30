9f1b19567c9cc94d2a885d1c8e10280d6a5d9f61
believe <believeosawaru2@gmail.com>
2024-12-30 02:36:22 +0100

    fix: update header navigation to use flex layout and improve accessibility with icons

--- js/header.js
+++ js/header.js
@@ -51,15 +51,34 @@ document.querySelector("nav").innerHTML = `
         <span><a href="https://happening.net/home">Happening</a></span>
 
             <div class="pc-menu home-pc-menu">
-                <a class="dis-flex" href="https://happening.net/home"><span class="material-symbols-outlined">
+                 <p><a class="dis-flex" href="https://happening.net/home"><span class="material-symbols-outlined">
                 home
-                </span>  Home</a>
-                <a href="https://happening.net/my-events">&#x1F4C5; Events</a>
-                <a href="https://happening.net/my-groups">&#x1F465; Groups</a>
-                <a href="https://happening.net/settings">&#x2699; Settings</a>
-                <a href="https://happening.net/tos">&#x1F4DC; Terms Of Service</a>
+                </span> <span>Home</span></a></p>
+
+                <p><a class="dis-flex" href="https://happening.net/blogs/my-feed"><span class="material-symbols-outlined">
+                 captive_portal
+                </span> <span>My Feed</span></a></p>
                 
-                <button class="first-btn"><a href="https://happening.net/log-out">Log Out</a></button>
+            <p><a class="dis-flex" href="https://happening.net/my-events"><span class="material-symbols-outlined">
+                event_available
+                </span> <span>Events</span></a></p>
+
+            <p><a class="dis-flex" href="https://happening.net/my-groups"><span class="material-symbols-outlined">
+                groups
+                </span> <span>Groups</span></a></p>
+
+            <p><a class="dis-flex" href="https://happening.net/settings"><span class="material-symbols-outlined">
+                settings
+                </span> <span>Settings</span></a></p>
+
+            <p><a class="dis-flex" href="https://happening.net/tos"><span class="material-symbols-outlined">
+                description
+                </span> <span>Terms Of Service</span></a></p>
+
+            <p><a class="dis-flex" href="https://happening.net/my-profile"><span class="material-symbols-outlined">
+                person
+                </span> <span>My Profile</span></a></p>
+            <button class="first-btn"><a href="https://happening.net/log-out">Log Out</a></button>
             </div>
 
             <i class="fa fa-bell" id="bell"><i class="alert"></i></i>
