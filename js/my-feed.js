059cff2785501b49a09e25cdfa68c5b941014b81
believe <believeosawaru2@gmail.com>
2025-01-05 01:54:01 +0100

    nknbj nk,

--- js/my-feed.js
+++ js/my-feed.js
@@ -30,7 +30,7 @@ async function myFeed() {
                 return formatter.format(postDate);
             }
 
-            function truncateContent(post, wordLimit = 2) {
+            function truncateContent(post, wordLimit = 15) {
                 const div = document.createElement('div');
                 div.textContent = post.content;
                 const content = div.textContent;
